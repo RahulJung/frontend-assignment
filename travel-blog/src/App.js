9d2808eaafe0cb1baf00f77701a84fc308a9f0ff
RahulJung <rahuljungchauhan@gmail.com>
2020-11-18 11:29:13 -0500

    - Setup Profile Page

--- travel-blog/src/App.js
+++ travel-blog/src/App.js
@@ -1,10 +1,28 @@
-import headshot from './images/headshot.jpg';
-import heroBackground from './images/hero-background.jpg';
-import './App.scss';
+import { LoremIpsum } from "react-lorem-ipsum";
+import headshot from "./images/headshot.jpg";
+import heroBackground from "./images/hero-background.jpg";
+import "./App.scss";
 
 function App() {
   return (
     <div className="app">
+      <div className="profile">
+        <div className="profile__backgroundImg">
+          <img className="profile__wallPaper" src={heroBackground} />
+        </div>
+        <div className="profile__aboutContainer">
+          <div className="profile__profileImage">
+            <img className="profile__headShot" src={headshot} />
+          </div>
+          <div className="profile__aboutMe">
+            <LoremIpsum p={5} />
+          </div>
+        </div>
+        <div className="profile__fotter">
+          <span>@ 2020 Travelize</span>
+          <span>Privacy Policy Terms and conditions</span>
+        </div>
+      </div>
     </div>
   );
 }
